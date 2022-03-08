024acc12bf29eeb7abc364f0605d520c80bb8f29
kari-osk <karinaosuka@gmail.com>
2022-03-07 21:01:28 -0300

    Navbar

--- dsmovie/frontend/src/App.tsx
+++ dsmovie/frontend/src/App.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import './App.css';
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </div>
+    <Navbar />
   );
 }
 
